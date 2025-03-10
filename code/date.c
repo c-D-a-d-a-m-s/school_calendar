294af19e381b63df3a11c23141dc84dd7d4ce044
c-D-a-d-a-m-s <christopheradams1022@gmail.com>
2025-03-09 21:42:10 -0500

    Add functions to check if a string is a valid date
    Added functions to check that a string represents a valid date and
    to store values represented in string date in a Date variable.

--- code/date.c
+++ code/date.c
@@ -91,3 +91,44 @@ bool check_valid_date(Date date) {
     return true;
 
 }
+
+void str_to_date(const char* const str, Date* date) {
+    /*Takes a string argument of the format "month/day/year", where month, day, and year are integers,
+    and passes the integers into the corresponding date members pointed to by the Date* argument.
+    WARNING: This function does not guarantee the values stored in *date correspond to a valid date. If a valid date
+             is required, use check_str_is_date() to check the string prior to calling this function.*/
+    char tempStr[strlen(str) + 1];
+    enum dateMember {YEAR, DAY, MONTH};
+    int dateMemberArr[3];
+    int dateMemberIdx = 0;
+
+    strncpy(tempStr, str, strlen(str) + 1);
+    for (int i = strlen(tempStr) - 1; i >= 0; i--) {
+        if (*(tempStr + i) == '/') {
+            *(tempStr + i) = '\0';
+            dateMemberArr[dateMemberIdx] = atoi(tempStr + i + 1);
+            dateMemberIdx++;
+        }
+    }
+    dateMemberArr[dateMemberIdx] = atoi(tempStr);
+
+    *date = (Date){.year = dateMemberArr[YEAR], .day = dateMemberArr[DAY], .month = dateMemberArr[MONTH]};
+}
+
+bool check_str_is_date(char* str) {
+    /*Returns true if a string represents a valid date in the format "month/day/year",
+    where month, day, and year are integers. Otherwise, returns false.*/
+    regex_t dateRegex;
+    Date date;
+    if(regcomp(&dateRegex, "^[0,1,2,3,4,5,6,7,8,9]*/[0,1,2,3,4,5,6,7,8,9]*/[0,1,2,3,4,5,6,7,8,9]*$", REG_NOSUB) != 0) die("check_str_is_date", "regcomp() failed to compile regex.");
+    if(regexec(&dateRegex, str, 0, NULL, 0) != 0) {
+        return false;
+    }
+    else {
+        str_to_date(str, &date);
+        if (!check_valid_date(date)) {
+            return false;
+        }
+    }
+    return true;
+}
--- code/date.h
+++ code/date.h
@@ -9,6 +9,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <regex.h>
 
 /*** data types */
 typedef struct {
@@ -23,5 +24,7 @@ void get_first_of_month(Date date, Date*);
 void get_last_of_month(Date date, Date*);
 int count_days(Date start, Date end);
 bool check_valid_date(Date date);
+void str_to_date(const char* const str, Date* date);
+bool check_str_is_date(char* str);
 
 #endif
